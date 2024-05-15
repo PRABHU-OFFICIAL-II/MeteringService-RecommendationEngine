096a31c5bb4cf30c5d3db82547e8f87f7eb699b4
PRABHU-OFFICIAL-II <prabhulitu2003@gmail.com>
2024-05-15 14:02:33 +0530

    initial commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
-// import 'package:recommendation_engine_ipu/display_data.dart';
-import 'package:recommendation_engine_ipu/upload_report.dart';
+import 'package:recommendation_engine_ipu/display_data.dart';
+// import 'package:recommendation_engine_ipu/upload_report.dart';
 
 void main() {
   runApp(const MyApp());
@@ -13,14 +13,13 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        title: 'Flutter Demo',
-        theme: ThemeData(
-          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-          useMaterial3: true,
-        ),
-        home: const Scaffold(
-          // body: DisplayData()),
-          body: UploadReportScreen(),
-        ));
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const Scaffold(body: DisplayData()),
+      // body: UploadReportScreen(),
+    );
   }
 }
